a212520a8e345446adf7d2c0acb9df1557e48edb
GuglielmoBini <guglielmobini@gmail.com>
2023-01-19 18:44:08 +0100

    created AppTitle

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default {
+  name: "App",
+  components: {
+    AppTitle
+  }
+}
+</script>
+
+<template>
+  <AppTitle />
+</template>
+
+<style>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: "AppTitle",
+    data() {
+        return {
+            title: 'La mia prima app con Vite!'
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+
+<style>
+header {
+    text-align: center;
+    margin-top: 50px;
+}
+
+h1 {
+    color: red;
+}
+</style>
